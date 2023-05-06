f7dc0a9d586ab016420147061ace40be07f86383
Nemanja <nemanja14zivanovic@gmail.com>
2023-05-06 13:16:01 +0200

    Fix Coinsnap settings page

--- includes/admin/settings/partials/page-coinsnap.php
+++ includes/admin/settings/partials/page-coinsnap.php
@@ -3,12 +3,8 @@
 if (!defined('ABSPATH')) {
   exit;
 }
-update_option('btcpw_coinsnap_website_id', '2Cf1T9H7UDFgmdKSnBhhKc8QuFeHcmsUfBjUG9nVijfG');
-update_option('btcpw_coinsnap_auth_key', '49f9e505c4786eebf22e8a8e069b7144269521d52774e221dcff7277ae41a0e0');
 $btcpw_coinsnap_key = get_option('btcpw_coinsnap_auth_key');
 $btcpw_coinsnap_website_id = get_option('btcpw_coinsnap_website_id');
-$btcpw_coinsnap_secret = get_option('btcpw_coinsnap_secret');
-$connection_status = $btcpw_coinsnap_website_id ? 'Connected' : '';
 
 ?>
 <style>
@@ -28,6 +24,7 @@ $connection_status = $btcpw_coinsnap_website_id ? 'Connected' : '';
     <table class="general_settings_table">
       <tbody>
         <form method="POST" action="options.php" id="btcpw_coinsnap_form">
+          <?php settings_fields('btcpw_coinsnap_settings'); ?>
           <tr class="odd">
             <td>
               <label for="btcpw_coinsnap_auth_key"><?php echo esc_html__('API Key', 'btcpaywall'); ?></label>
@@ -44,24 +41,8 @@ $connection_status = $btcpw_coinsnap_website_id ? 'Connected' : '';
               <input type="text" name="btcpw_coinsnap_website_id" id="btcpw_coinsnap_website_id" value="<?php echo esc_attr($btcpw_coinsnap_website_id); ?>">
             </td>
           </tr>
-          <tr class="odd">
-            <td>
-              <label for="btcpw_coinsnap_secret"><?php echo esc_html__('Secret', 'btcpaywall'); ?></label>
-            </td>
-            <td>
-              <input type="text" name="btcpw_coinsnap_secret" id="btcpw_coinsnap_secret" value="<?php echo esc_attr($btcpw_coinsnap_secret); ?>">
-            </td>
-          </tr>
-          <tr class="even">
-            <td>
-              <label for="btcpw_coinsnap_connection_status"><?php echo esc_html__('Connection status', 'btcpaywall'); ?></label>
-            </td>
-            <td>
-              <input type="text" disabled id="btcpw_coinsnap_connection_status" value="<?php echo esc_attr($connection_status); ?>">
-            </td>
-          </tr>
           <tr class="even btcpw_general_settings_buttons">
-            <td><button id="coinsnap-connect-website" class="button button-secondary btcpw_button"><?php echo esc_html('Connect', 'btcpaywall'); ?></button></td>
+            <td colspan=2><button class="button button-primary btcpw_button" type="submit">Save</button></td>
           </tr>
         </form>
       </tbody>
